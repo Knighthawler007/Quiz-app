cea5f872739eed5d32d15e2f3cd56ccf49c66526
Madhurya23 <madhuryanaha@gmail.com>
2024-03-03 18:08:03 -0800

    connected page 2 with page 3

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const pathfinder=path.join(__dirname,'client')
 app.use(bodyparser.urlencoded({extended:true}))
 
 
-app.get("/",(req,res)=>{
+app.get("/start",(req,res)=>{
     res.sendFile(`${pathfinder}/index.html`)
 });
 app.post("/start",(req,res)=>{
@@ -15,12 +15,20 @@ app.post("/start",(req,res)=>{
     res.redirect("/start2");
 });
 app.get("/start2",(req,res)=>{
-    res.sendFile(`${pathfinder}/index2.html`)
+    res.sendFile(`${pathfinder}/Topics.html`)
     
 });
 app.post("/start2",(req,res)=>{
     const body2=req.body;
     console.log("submitted with:",body2);
+    res.redirect("/start3");
+});
+app.get("/start3",(req,res)=>{
+    res.sendFile(`${pathfinder}/index2.html`)
+});
+app.post("/start3",(req,res)=>{
+    const body3=req.body;
+    console.log("submitted with",body3);
     res.send("submitted");
 });
 app.listen(4000,()=>{
