543bbf2bf2b9194d4e3096baee3af0a6042af78d
Madhurya23 <madhuryanaha@gmail.com>
2024-03-03 23:42:06 -0800

    linked pages

--- index.js
+++ index.js
@@ -19,16 +19,67 @@ app.get("/start2",(req,res)=>{
     
 });
 app.post("/start2",(req,res)=>{
-    const body2=req.body;
-    console.log("submitted with:",body2);
-    res.redirect("/start3");
+   const topic=req.body;
+   switch(topic)
+   {
+        case "code":
+            res.redirect("/code");
+            break;
+        case "sport":
+            res.redirect("/sport");
+            break;
+        case "sci":
+            res.redirect("/sci");
+            break;
+        case "lit":
+            res.redirect("/lit");
+            break;
+        case "curr":
+            res.redirect("/curr");
+            break;
+        default:
+            res.redirect("/sci");
+
+   }
 });
-app.get("/start3",(req,res)=>{
-    res.sendFile(`${pathfinder}/index2.html`)
+app.get("/code",(req,res)=>{
+    res.sendFile(`${pathfinder}/coding.html`)
 });
-app.post("/start3",(req,res)=>{
+app.post("/code",(req,res)=>{
     const body3=req.body;
     console.log("submitted with",body3);
+    res.send("submitted");
+});
+app.get("/curr",(req,res)=>{
+    res.sendFile(`${pathfinder}/Current.html`)
+});
+app.post("/curr",(req,res)=>{
+    const body4=req.body;
+    console.log("submitted with",body4);
+    res.send("submitted");
+});
+app.get("/sport",(req,res)=>{
+    res.sendFile(`${pathfinder}/Sports.html`)
+});
+app.post("/sport",(req,res)=>{
+    const body5=req.body;
+    console.log("submitted with",body5);
+    res.send("submitted");
+});
+app.get("/sci",(req,res)=>{
+    res.sendFile(`${pathfinder}/Science.html`)
+});
+app.post("/sci",(req,res)=>{
+    const body6=req.body;
+    console.log("submitted with",body6);
+    res.send("submitted");
+});
+app.get("/lit",(req,res)=>{
+    res.sendFile(`${pathfinder}/Literature.html`)
+});
+app.post("/lit",(req,res)=>{
+    const body7=req.body;
+    console.log("submitted with",body7);
     res.send("submitted");
 });
 app.listen(4000,()=>{
